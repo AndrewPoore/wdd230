a0983350a9e5467ccaa1106ca7214fae4ac0d869
AndrewPoore <andrewpoore27@gmail.com>
2022-02-03 17:54:35 -0700

    DOM manipulation activity

---
+++ lesson5/bom/js/bom.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('#favchap');
+
+const button = document.querySelector('#btn1');
+
+const output = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = "❌";
+    document.getElementById('favchap').value = "";
+    li.append(deleteButton);
+    output.append(li);
+    deleteButton.addEventListener('click', () => {
+        output.removeChild(li);
+        input.focus();
+    });
+});
